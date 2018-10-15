a6e935fc9c90a8b21fb3e0849972304c16d3af80
Cavallucci Martina <martina.cavallucci@studio.unibo.it>
2018-10-15 15:21:36 +0200

    Add a form with button to show a plot

---
+++ RDotNet-Example/RDotNet-Example/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Forms;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Printing;
+using System;
+
+namespace RDotNet
+{
+    internal class Form1 : Form
+    {
+
+        private Button printPlot = new Button();
+        private Button printData = new Button();
+
+
+        public Form1()
+        {
+            printPlot.Text = "Print Avg and Std";
+            printPlot.Click += new EventHandler(printPlot_Click);
+
+            printPlot.Text = "Print Plot";
+            printPlot.Click += new EventHandler(printData_Click);
+
+            this.Controls.Add(printPlot);
+            this.Controls.Add(printData);
+        }
+
+        void printPlot_Click(object sender, EventArgs e)
+        {
+            PictureBox imageControl = new PictureBox();
+            imageControl.Width = 800;
+            imageControl.Height = 800;
+            this.Size = new Size(750, 750);
+            Bitmap image = new Bitmap("myplot.png");
+            imageControl.Dock = DockStyle.Fill;
+            imageControl.Image = (Image)image;
+            Controls.Add(imageControl);
+        }
+        void printData_Click(object sender, EventArgs e)
+        {
+            
+          
+        }
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(747, 534);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
--- RDotNet-Example/RDotNet-Example/Program.cs
+++ RDotNet-Example/RDotNet-Example/Program.cs
@@ -6,6 +6,10 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Printing;
 namespace RDotNet
 {
 
@@ -18,20 +22,20 @@ namespace RDotNet
 
             // There are several options to initialize the engine, but by default the following suffice:
             REngine engine = REngine.GetInstance();
-
             engine.Initialize(); // required since v1.5                            
 
             // some random weight samples 
-            double[] samples = new double[] { 3.2, 3.6, 3.2, 1.7, 0.8, 2.9, 2, 1.4, 1.2, 2.1, 2.5, 3.9, 3.7, 2.4, 1.5, 0.9, 2.5, 1.7, 2.8, 2.1, 1.2 };
-            double[] second = new double[] { 2, 3, 3.2, 4.7, 5.8, 3.9, 2, 8.4, 5.2, 4.1, 2.5, 3.9, 5, 2.4, 3.5, 0.9, 2.5, 2.7, 2.8, 2.1, 1.2 };
+            double[] weight = new double[] { 3.2, 3.6, 3.2, 1.7, 0.8, 2.9, 2, 1.4, 1.2, 2.1, 2.5, 3.9, 3.7, 2.4, 1.5, 0.9, 2.5, 1.7, 2.8, 2.1, 1.2 };
+            double[] lenght = new double[] { 2, 3, 3.2, 4.7, 5.8, 3.9, 2, 8.4, 5.2, 4.1, 2.5, 3.9, 5, 2.4, 3.5, 0.9, 2.5, 2.7, 2.8, 2.1, 1.2 };
 
             // introduce the samples into R
-            engine.SetSymbol("samples", engine.CreateNumericVector(samples));
-            engine.SetSymbol("second", engine.CreateNumericVector(second));
+            engine.SetSymbol("weight", engine.CreateNumericVector(weight));
+            engine.SetSymbol("lenght", engine.CreateNumericVector(lenght));
 
-            // set the weights as a data frame (regular R syntax in string)
-            engine.Evaluate("df <- data.frame(id=c(1:length(samples)), weight = samples,lenght = second )");
+            // set the weights and lenghts as a data frame (regular R syntax in string)
+            engine.Evaluate("df <- data.frame(id=c(1:length(weight)), weight = weight,lenght = lenght )");
 
+         
             // evaluate and retrieve mean 
             double avg = engine.Evaluate("mean(df$weight)").AsNumeric().ToArray()[0];
             // same for standard deviation
@@ -40,42 +44,40 @@ namespace RDotNet
             // print output in console
             System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("en-gb");
 
+            //Show in console the weight and lenght data
             Console.WriteLine(string.Format("Weights: ({0})", string.Join(",",
-                samples.Select(f => f.ToString(ci)) // LINQ expression
+                weight.Select(f => f.ToString(ci)) // LINQ expression
                 )));
-            Console.WriteLine(string.Format("Sample size: {0}", samples.Length));
+            Console.WriteLine(string.Format("Length: ({0})", string.Join(",",
+            lenght.Select(f => f.ToString(ci)) // LINQ expression
+            )));
+            Console.WriteLine(string.Format("Sample size: {0}", weight.Length));
             Console.WriteLine(string.Format(ci, "Average: {0:0.00}", avg));
             Console.WriteLine(string.Format(ci, "Standard deviation: {0:0.00}", std));
 
             var result = engine.Evaluate("lm(df$weight ~ df$lenght)");
             engine.SetSymbol("result", result);
-
             var coefficients = result.AsList()["coefficients"].AsNumeric().ToList();
             double r2 = engine.Evaluate("summary(result)").AsList()["r.squared"].AsNumeric().ToList()[0];
             double intercept = coefficients[0];
             double slope = coefficients[1];
-            Console.WriteLine("Intercetta:" + intercept.ToString());
+            Console.WriteLine("Intercept:" + intercept.ToString());
             Console.WriteLine("slope:" + slope);
             Console.WriteLine("r2:" + r2);
-            Tuple.Create(intercept, slope, r2);
-            string fileName;
-
-            fileName = "myplot.png";
+           
+            string fileName = "myplot.png";
 
             CharacterVector fileNameVector = engine.CreateCharacterVector(new[] { fileName });
             engine.SetSymbol("fileName", fileNameVector);
-
+            
+            engine.Evaluate("png(filename=fileName, width=6, height=6, units='in', res=100)");
             engine.Evaluate("reg <- lm(df$weight ~ df$lenght)");
             engine.Evaluate("plot(df$weight ~ df$lenght)");
             engine.Evaluate("abline(reg)");
-            engine.Evaluate("png(filename=fileName, width=6, height=6, units='in', res=100)");
-            engine.Evaluate("plot(df$weight ~ df$lenght)");
             engine.Evaluate("dev.off()");
             //The file will save in debug directory
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-
+      
+            Application.Run(new Form1());
             // After disposing of the engine, you cannot reinitialize nor reuse it
             engine.Dispose();
 
